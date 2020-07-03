810e6f1f59461bd3fa37ba1e96dd9054edcf4f08
ashu5398 <agrawalashu945@gmail.com>
2020-07-03 15:14:10 +0530

    hjh

--- app/src/main/java/com/indiagold/samplemessage/MainActivity.java
+++ app/src/main/java/com/indiagold/samplemessage/MainActivity.java
@@ -20,74 +20,18 @@ import com.google.firebase.inappmessaging.model.InAppMessage;
 
 import java.util.concurrent.Executor;
 
-public class MainActivity extends AppCompatActivity implements FirebaseInAppMessagingClickListener, FirebaseInAppMessagingImpressionListener, FirebaseInAppMessagingDisplay {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseApp.initializeApp(MainActivity.this);
-        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener( new OnSuccessListener<InstanceIdResult>() {
-            @Override
-            public void onSuccess(InstanceIdResult instanceIdResult) {
-                String deviceToken = instanceIdResult.getToken();
-                Log.d("Instance ID ",deviceToken);
-            }
-        });
 
         final InAppMessageClick inAppMessageClick = new InAppMessageClick();
         FirebaseInAppMessaging.getInstance().addClickListener(inAppMessageClick);
+        FirebaseInAppMessaging.getInstance().addImpressionListener(inAppMessageClick);
+        FirebaseInAppMessaging.getInstance().setMessageDisplayComponent(inAppMessageClick);
 
-        InAppMessageClick inAppMessageClick2 = new InAppMessageClick();
-        FirebaseInAppMessaging.getInstance().addImpressionListener(inAppMessageClick2);
-
-        InAppMessageClick inAppMessageClick3 = new InAppMessageClick();
-        FirebaseInAppMessaging.getInstance().setMessageDisplayComponent(inAppMessageClick3);
-
-
-        FirebaseInAppMessaging.getInstance().addClickListener(new FirebaseInAppMessagingClickListener() {
-            @Override
-            public void messageClicked(@NonNull InAppMessage inAppMessage, @NonNull Action action) {
-                Log.d("Message Click", "Executor");
-            }
-        });
-
-        FirebaseInAppMessaging.getInstance().addClickListener(inAppMessageClick2, new Executor() {
-            @Override
-            public void execute(Runnable command) {
-                Log.d("Impression", "Executor");
-            }
-        });
-
-
-        FirebaseInAppMessaging.getInstance().addClickListener(inAppMessageClick3, new Executor() {
-            @Override
-            public void execute(Runnable command) {
-                Log.d("Message Display", "Executor");
-            }
-        });
-
-
-
-
-}
-
-    String TAG = "InAppMessageClick";
-    @Override
-    public void messageClicked(InAppMessage inAppMessage, Action action) {
-        String url = action.getActionUrl();
-        Log.d(TAG, "Action URL : "+url);
     }
-    @Override
-    public void impressionDetected(InAppMessage inAppMessage) {
-        Log.d(TAG, "impressionDetected "+inAppMessage.getCampaignMetadata().getCampaignName());
-        Log.d(TAG, "metadata "+inAppMessage.getCampaignMetadata());
-    }
-    @Override
-    public void displayMessage(InAppMessage inAppMessage, FirebaseInAppMessagingDisplayCallbacks
-            firebaseInAppMessagingDisplayCallbacks) {
-        Log.d(TAG, "displayMessage Action URL : "+inAppMessage.getCampaignMetadata().getCampaignName());
-    }
-
 
 }
